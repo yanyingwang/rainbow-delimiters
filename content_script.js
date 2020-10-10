4779deeb45e4587a118983afb2a4bb68cc902226
Yanying Wang <yanyingwang1@gmail.com>
2020-10-10 18:01:11 +0800

    pushing code

--- content_script.js
+++ content_script.js
@@ -3,7 +3,32 @@ console.log("========from content_script.js");
 console.log("========from content_script.js");
 console.log("========from content_script.js");
 console.log("========from content_script.js");
+console.log(jQuery().jquery);
 
-$('a').filter(function(){
+colors = [
+  "#458588",
+  "#b16286",
+  "DarkSeaGreen",
+  "IndianRed",
+  "DarkOrchid",
+  "SteelBlue",
+  "DeepPink2",
+  "OliveDrab",
+  "dark red"
+]
+
+
+
+
+$('a').filter(function() {
     return this.host !== location.host
 }).attr('target','_blank');
+
+
+$('blockquote').find("span.RktPn").each(function() {
+  debugger;
+}).attr('target','_blank');
+
+
+// $('blockquote').find("span.RktPn").filter(function() {
+// }).attr('target','_blank');
