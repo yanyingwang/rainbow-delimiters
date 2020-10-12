455fc94abf73818a2e65f6808e7da21b9b5090f1
Yanying Wang <yanyingwang1@gmail.com>
2020-10-12 17:40:16 +0800

    add color id to html

--- content_script.js
+++ content_script.js
@@ -1,11 +1,12 @@
 
 console.log("========from content_script.js");
-console.log("========from content_script.js");
-console.log("========from content_script.js");
-console.log("========from content_script.js");
 console.log(jQuery().jquery);
+$('a').filter(function() {
+    return this.host !== location.host
+}).attr('target','_blank');
 
-colors = [
+
+var colors = [
   "#458588",
   "#b16286",
   "DarkSeaGreen",
@@ -17,18 +18,44 @@ colors = [
   "dark red"
 ]
 
+var colorized = [];
+$('blockquote').find("span.RktPn").each(function(i) {
+  if (this.textContent === "(") {
+    let c = colors[colorized.length];
+    let id = c.replace("#", "-").replace(" ", "-")
+    $(this).css("color", c);
+    $(this).attr('id', `color-${id}`);
+    colorized.push(c);
+    // $(this).css("font-weight", "bolder");
+  };
+
+  if (this.textContent === ")") {
+    let c = colorized.pop()
+    let id = c.replace("#", "-").replace(" ", "-")
+    $(this).css("color", c);
+    $(this).attr('id', `color-${id}`);
+    // $(this).css("font-weight", "bolder");
+  };
+});
+
+var colorized = [];
+$('blockquote').find("span.RktPn").each(function(i) {
+  if (this.textContent === "[") {
+    let c = colors[colorized.length];
+    $(this).css("color", c);
+    colorized.push(c);
+    // $(this).css("font-weight", "bolder");
+  };
+
+  if (this.textContent === "]") {
+    $(this).css("color", colorized.pop());
+    // $(this).css("font-weight", "bolder");
+  };
+});
 
 
 
-$('a').filter(function() {
-    return this.host !== location.host
-}).attr('target','_blank');
-
-
-$('blockquote').find("span.RktPn").each(function() {
-  debugger;
-}).attr('target','_blank');
-
-
-// $('blockquote').find("span.RktPn").filter(function() {
-// }).attr('target','_blank');
+// $("span.RktPn").mouseover(function(i) {
+//   console.log("=====detected!!!!");
+//   debugger;
+// });
