ca18e7aea31335ffcfe8f1017a3d366524103b1c
Yanying Wang <yanyingwang1@gmail.com>
2021-05-24 14:30:50 +0800

    fix desc

--- racket/info.rkt
+++ racket/info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base" "scribble-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/rainbow-delimiters.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Colorizing matched brackets of codes on HTML pages of your browsers.")
 (define version "0.1")
 (define pkg-authors '("Yanying Wang"))
